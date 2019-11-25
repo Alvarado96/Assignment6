69b0e62dea2a2ee796e600794b4b20e36222efcd
fgd806 <fgd806@my.utsa.edu>
2019-11-25 21:06:14 +0000

    Initial commit

--- upload/views.py
+++ upload/views.py
@@ -2,12 +2,14 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
 from .forms import UploadFileForm
+from django.core.exceptions import ValidationError
+
 import os
 
-from django.core.exceptions import ValidationError
 attempted = 0
 successful = 0
 msg = ""
+
 def upload_file(request):
     global attempted
     global successful
@@ -15,25 +17,24 @@ def upload_file(request):
     msg = ""
     attempted = 0
     successful = 0
-    if request.method == 'POST':
-        '''form = UploadFileForm(request.POST, request.FILES)'''
-        '''if form.is_valid():'''
-        for filename, file in request.FILES.items():
-            # Do the incrementation
-            attempted+=1
 
+    if request.method == 'POST':
+        #get the list of uploaded files
+        for filename, file in request.FILES.items():
+            attempted+=1
             
+            #if the file meets any of the requirements then increment successful
             if(handle_uploaded_file(request.FILES[filename])):
                 successful+=1
+
         msg+="<p>Upload Summary: " + str(successful) + " successful out of " + str(attempted) + "."
         template = loader.get_template('upload/results.html')
         context = {'msg': msg}
         return HttpResponse(template.render(context, request))
-            #return HttpResponseRedirect('/successful/url/')
+
     else:
         form = UploadFileForm()
     return render(request, 'upload/upload.html')
-    #return render(request, 'upload/upload.html',) #{'form' : form})
 
 def handle_uploaded_file(f):
     global msg
@@ -50,13 +51,12 @@ def handle_uploaded_file(f):
         msg+= "<p>Sorry, only JPG and PNG files are allowed, not <b>" + str(f.name) + "</b>"
         return False
 
-    # Do HTISSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
     filesize = f.read()
     if len(filesize) > maximum:
         msg+="<p>Sorry, file <b>" + str(f.name) + "</b> is too large (" +                     str(len(filesize)) + " > " + str(maximum) + ")."
         return False
 
-    with open('/home/webtech/django/assignment6/media/' + str(f.name), 'wb+') as destination:
+    with open(file_path, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
     msg+="<p>The file <b>" + str(f.name) + "</b> has been uploaded (bytes = " +             str(len(filesize)) + ")."
